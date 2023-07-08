28fc39d29aebe55381cf11e28db917f9df3276b6
Kevin Pham <phamtkevin@gmail.com>
2023-07-08 11:35:13 -0700

    Installed required libraries

---
+++ virtual_assistant.py
@@ -0,0 +1,16 @@
+''' 
+python libraries installed:
+- pyttsx3 (allow python to 'talk' to us)
+- SpeechRecognition (speech to text translation)
+- pywhatkit (open and access websites)
+- yfinance (Yahoo Finance stock market access)
+- pyjokes (library of jokes)
+'''
+
+
+import pyttsx3
+import speech_recognition as sr
+import pywhatkit 
+import yfinance as yf
+import pyjokes
+
